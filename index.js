b9b455362a53eddf72ee163217091f5b3b317d99
wisnusabda <manusiq630@gmail.com.com>
2020-12-27 05:03:42 +0800

    Discord-Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client;
+
+const token = 'NzkxNjg5MTM0MjM3ODEwNzE5.X-Sz9g._fYcDHfh3K7C_zS7R5AyucRQiwE';
+var PREFIX = '!';
+
+bot.on('ready', () => {
+    console.log('Bot Online')
+})
+
+bot.on('message', message => {
+    if (message.content === "!helpbot") {
+        message.channel.send({embed: {
+            title: "Embed",
+            color: 10181046,
+            description: "!helpbot h3h3",
+           fields: [{
+               name : "Command #1",
+               value : "You can put all the *usual* **__Markdown__** inside of them."
+           }]
+          }});
+    };
+});
+  
+
+bot.login(token);
+
